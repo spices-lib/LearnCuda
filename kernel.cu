243ad4f1f81ffd69567f393bec115b6d6e33c660
Spices <spiecs@outlook.com>
2026-05-26 22:31:49 +0800

    init

---
+++ kernel.cu
@@ -0,0 +1,68 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <memory>
+
+constexpr uint32_t SIZE = 1024;
+
+__global__ void test01()
+{
+	// print the blocks and threads IDs
+	int warp_ID_Value = 0;
+	warp_ID_Value = threadIdx.x / 32;
+	printf("\nThe block ID is %d -- The thread ID is %d -- The warp id %d\n", blockIdx.x, threadIdx.x, warp_ID_Value);
+}
+
+__global__ void vectorAdd(int* A, int* B, int* C, int n)
+{
+	int i = threadIdx.x;
+	C[i] = A[i] + B[i];
+}
+
+int main()
+{
+	int* A, * B, * C;
+	int* d_A, * d_B, * d_C;
+	int size = SIZE * sizeof(int);
+
+	A = (int*)malloc(size);
+	B = (int*)malloc(size);
+	C = (int*)malloc(size);
+
+	cudaMalloc((void**)&d_A, size);
+	cudaMalloc((void**)&d_B, size);
+	cudaMalloc((void**)&d_C, size);
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		A[i] = i;
+		B[i] = SIZE - i;
+	}
+
+	cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+
+	vectorAdd << <1, SIZE >> > (d_A, d_B, d_C, SIZE);
+
+	cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+
+	printf("\nExecution finished\n");
+	for (int i = 0; i < SIZE; ++i)
+	{
+		printf("%d + %d = %d", A[i], B[i], C[i]);
+		printf("\n");
+	}
+
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+
+	free(A);
+	free(B);
+	free(C);
+
+	cudaDeviceSynchronize();
+
+	return 0;
+}
